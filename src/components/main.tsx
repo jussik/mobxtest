6be4a8ecf7a83ea76b5e727f9ec38c2337eed123
Jussi Kosunen <deathcarrot@gmail.com>
2018-02-25 19:00:57 +0200

    store + input

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
-import { action, observable } from "mobx";
-import { observer } from "mobx-react";
+import { action, observable, computed } from "mobx";
+import { observer, Provider, inject } from "mobx-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckSquare, faSquare } from "@fortawesome/fontawesome-free-regular";
+
+class RowStore {
+    @observable readonly rows: TableRow[] = [];
+    add(text: string, active: boolean = false) {
+        this.rows.push(new TableRow(text, active));
+    }
+}
 
 class TableRow {
     private static idCounter = 0;
@@ -14,7 +21,7 @@ class TableRow {
         this.text = text;
         this.active = active;
     }
-    @action toggle() {
+    @action.bound toggle() {
         this.active = !this.active;
     }
 }
@@ -23,46 +30,70 @@ class TableRow {
 class RowComponent extends React.Component<{ row: TableRow }, {}> {
     render() {
         const row = this.props.row;
-        return <tr style={{cursor:"pointer"}} onClick={()=>row.toggle()}>
+        return <tr style={{cursor:"pointer"}} onClick={row.toggle}>
             <td><FontAwesomeIcon icon={row.active ? faCheckSquare : faSquare} /></td>
             <td>{row.text}</td>
         </tr>;
     }
 }
 
+@inject("rowStore")
 @observer
-export default class Main extends React.Component<{}, {}> {
-    rows: TableRow[];
-    constructor(props: {}) {
-        super(props);
-        this.rows = [
-            new TableRow("A test"),
-            new TableRow("Another test"),
-            new TableRow("And another one"),
-            new TableRow("Zoop"),
-            new TableRow("Bop")
-        ];
+class RowForm extends React.Component<{ rowStore?: RowStore }, {}> {
+    @observable private name: string = "";
+    @action.bound nameChanged(e: React.FormEvent<HTMLInputElement>) {
+        this.name = e.currentTarget.value;
+    }
+    @action.bound submit(e: React.FormEvent<HTMLFormElement>) {
+        this.props.rowStore!.add(this.name);
+        this.name = "";
+        e.preventDefault();
     }
     render() {
-        return <section className="section">
-            <ul className="tabs">
-                <li className="is-active"><a>Table test</a></li>
-            </ul>
-            <div>
-                <h1 className="title">Testing...</h1>
-                <table className="table is-fullwidth is-striped is-hoverable">
-                    <thead>
-                        <tr>
-                            <th style={{ width: "28px" }} />
-                            <th>Name</th>
-                        </tr>
-                    </thead>
-                    <tbody>{
-                        this.rows.map(r => <RowComponent key={r.id} row={r} />)
-                    }</tbody>
-                </table>
-                <div>{this.rows.reduce((s, r) => s += r.active ? 1 : 0, 0)} of {this.rows.length} selected</div>
+        return <form className="field has-addons" onSubmit={this.submit}>
+            <div className="control">
+                <input className="input" type="text" placeholder="Name" required
+                    value={this.name} onChange={this.nameChanged} />
             </div>
-        </section>;
+            <div className="control">
+                <button type="submit" className="button">Add</button>
+            </div>
+        </form>;
+    }
+}
+
+@observer
+export default class Main extends React.Component<{}, {}> {
+    readonly store = new RowStore();
+    render() {
+        return <Provider rowStore={this.store}>
+            <section className="section">
+                <div className="tabs">
+                <ul>
+                    <li className="is-active"><a>Table test</a></li>
+                </ul>
+                </div>
+                <div>
+                    <table className="table is-fullwidth is-striped is-hoverable">
+                        <thead>
+                            <tr>
+                                <th style={{ width: "28px" }} />
+                                <th>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>{
+                            this.store.rows.map(r => <RowComponent key={r.id} row={r} />)
+                        }</tbody>
+                    </table>
+                    <RowForm />
+                    <div>{
+                        this.store.rows.reduce((s, r) => s += r.active ? 1 : 0, 0)
+                    } of {
+                        this.store.rows.length
+                    } selected
+                    </div>
+                </div>
+            </section>
+        </Provider>;
     }
 }
