5600ab26c9612c2f1d42fdfd67ae0fa10bd6993b
Jussi Kosunen <deathcarrot@gmail.com>
2018-05-19 19:18:10 +0300

    lazy load tabs

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -3,38 +3,58 @@ import { observable, action } from "mobx";
 import { observer, Provider } from "mobx-react";
 
 import { RowStore } from "../common/RowStore";
-import TableTab from "./TableTab";
-import OtherTab from "./OtherTab";
+
+interface TabModule {
+    default: any
+}
+
+class Tab {
+    @observable component: any = null;
+    constructor(
+        readonly title: string,
+        readonly moduleFetcher: () => Promise<TabModule>
+    ) { }
+    @action.bound private moduleLoaded(mod: TabModule) {
+        this.component = mod.default;
+    }
+    load() {
+        if (this.component == null) {
+            this.moduleFetcher().then(this.moduleLoaded);
+        }
+    }
+}
 
 @observer
 export default class MainPage extends React.Component<{}, {}> {
     readonly store = new RowStore();
-    readonly tabs = ["Table test", "Other"];
-    @observable currentTab = 0;
-    @action.bound setTab(tab: number) {
+    private readonly tabs = [
+        new Tab("Table test", () => import("./TableTab")),
+        new Tab("Other", () => import("./OtherTab"))
+    ];
+    @observable private currentTab: Tab;
+
+    constructor(props: {}, context: any) {
+        super(props, context);
+        this.currentTab = this.tabs[0];
+        this.currentTab.load();
+    }
+    @action.bound private setTab(tab: Tab) {
         this.currentTab = tab;
+        tab.load();
     }
     render() {
-        let tab: any;
-        switch (this.currentTab) {
-            case 0:
-                tab = <TableTab/>;
-                break;
-            case 1:
-                tab = <OtherTab/>;
-                break;
-            default:
-                throw new Error(`Unhandled tab: ${this.currentTab}`);
-        }
-
-        const tabHeaders = this.tabs.map((t, i) => <li key={i} className={this.currentTab === i ? "is-active" : ""} onClick={() => this.setTab(i)}><a>{t}</a></li>);
+        const tab = this.currentTab;
+        const tabHeaders = this.tabs.map((t, i) =>
+            <li key={i} className={tab === t ? "is-active" : ""} onClick={() => this.setTab(t)}>
+                <a>{t.title}</a>
+            </li>);
 
         return <Provider rowStore={this.store}>
             <section className="section">
                 <div className="tabs">
                     <ul>{tabHeaders}</ul>
                 </div>
-                {tab}
+                {tab.component ? React.createElement(tab.component) : <span>Loading...</span>}
             </section>
         </Provider>;
     }
